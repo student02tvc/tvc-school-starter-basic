bd88075aeb34486b7481bca8c8c460a68745ecbc
klequis <klequis@gmail.com>
2017-07-08 17:46:19 -0700

    clean-up

--- README.md
+++
@@ -1,4 +0,0 @@
-# TVC School Lessons
-
-## Technical Notes
-- Method of watching html files and reloading via Webpack is based on [this post](https://github.com/jantimon/html-webpack-plugin/issues/100)
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import '../css/style.css'
-// require('./style.css')
-
-
-if (process.env.NODE_ENV !== 'production') {
-    require('file-loader!../index.html')
-    // require('file-loader!../public/03.html-challenge-01.html')
-
-}
